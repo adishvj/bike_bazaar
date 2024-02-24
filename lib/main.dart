0aed1213f566c5a022d30e7c3b4efa6d69d7867e
adish vijayan p k <2001adish@gmail.com>
2024-02-25 02:36:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:bike_bazaar/screens/login.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(MaterialApp(home:login()));
+
+
+}
+
+
+
+
